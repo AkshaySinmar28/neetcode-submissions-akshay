712a3d36362dd1a3858dfb3205bd0d935bbade84
Akshay Singh <119798884+AkshaySinmar28@users.noreply.github.com>
2026-04-22 23:28:22 +0530

    Add: house-robber - submission-8

---
+++ Data Structures & Algorithms/house-robber/submission-8.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int prev1 = 0;
+        int prev2 = 0;
+        for(int i=0; i < n; i++){
+            int temp = Math.max(prev1+nums[i],prev2);
+            prev1 = prev2;
+            prev2 =temp;
+        }
+        return prev2;
+    }
+}
